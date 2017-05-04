ddf2dced6a54c0d57aaa4a447f484c29c530edbb
treviallen <trevor.allen@canada.ca>
2017-05-04 21:52:00 +1000

    adding cpt2colormap funtion to mapping tools

--- mapping_tools.py
+++ mapping_tools.py
@@ -727,6 +727,109 @@ def get_WGS84_area(geom):
     #print geom_area.area / 1000000.
     
     return geom_area.area / 1000000.
+    
+'''
+code below stolen from:
+http://wiki.scipy.org/Cookbook/Matplotlib/Loading_a_colormap_dynamically
+'''
+
+def cpt2colormap(fileName, ncolours, **kwargs):
+
+    import colorsys
+    from numpy import array, interp, linspace
+    from pylab import matplotlib
+
+    # get kwargs
+    rev = False
+    for key in ['rev']:
+        if key in kwargs:
+            # set fault type
+            if key == 'rev':
+                rev = kwargs[key]
+
+    try:
+        f = open(fileName)
+    except:
+        print "file ",fileName, "not found"
+        return None
+
+    lines = f.readlines()
+    f.close()
+
+    x = []
+    r = []
+    g = []
+    b = []
+    colorModel = "RGB"
+    for l in lines:
+        ls = l.split()
+        if l[0] == "#":
+           if ls[-1] == "HSV":
+               colorModel = "HSV"
+               continue
+           else:
+               continue
+        if ls[0] == "B" or ls[0] == "F" or ls[0] == "N":
+           pass
+        else:
+            x.append(float(ls[0]))
+            r.append(float(ls[1]))
+            g.append(float(ls[2]))
+            b.append(float(ls[3]))
+            xtemp = float(ls[4])
+            rtemp = float(ls[5])
+            gtemp = float(ls[6])
+            btemp = float(ls[7])
+
+    x.append(xtemp)
+    r.append(rtemp)
+    g.append(gtemp)
+    b.append(btemp)
+
+#    nTable = len(r)
+    x = array( x )
+    r = array( r )
+    g = array( g )
+    b = array( b )
+    if colorModel == "HSV":
+       for i in range(r.shape[0]):
+           rr,gg,bb = colorsys.hsv_to_rgb(r[i]/360.,g[i],b[i])
+           r[i] = rr ; g[i] = gg ; b[i] = bb
+    if colorModel == "HSV":
+       for i in range(r.shape[0]):
+           rr,gg,bb = colorsys.hsv_to_rgb(r[i]/360.,g[i],b[i])
+           r[i] = rr ; g[i] = gg ; b[i] = bb
+    if colorModel == "RGB":
+        r = r/255.
+        g = g/255.
+        b = b/255.
+
+    # reverse order
+    if rev == True:
+        r = r[::-1]
+        g = g[::-1]
+        b = b[::-1]
+
+    # interpolate to ncolours
+    xx = linspace(x[0], x[-1], ncolours)
+    r = interp(xx, x, r)
+    g = interp(xx, x, g)
+    b = interp(xx, x, b)
+    x = xx
+
+    xNorm = (x - x[0])/(x[-1] - x[0])
+
+    red = []
+    blue = []
+    green = []
+    for i in range(len(x)):
+        red.append([xNorm[i],r[i],r[i]])
+        green.append([xNorm[i],g[i],g[i]])
+        blue.append([xNorm[i],b[i],b[i]])
+
+    colorDict = {"red":red, "green":green, "blue":blue}
+
+    return matplotlib.colors.LinearSegmentedColormap('my_colormap',colorDict,ncolours), xx
 
 
 # !!!!!!!!!!code to convert projections!!!!!!!!!!
