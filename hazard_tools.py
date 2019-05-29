33ee828922a56cc55c5bc664f4f1e333c498ce67
Trevor Allen <trev@Trevors-MacBook-Air.local>
2019-05-29 21:39:57 +1000

    fixes to haz tools

--- hazard_tools.py
+++ hazard_tools.py
@@ -38,16 +38,15 @@ def get_probability_from_obs_and_return_period(return_period, investigation_time
     from numpy import exp
     return 1 - exp(-investigation_time/return_period)
 
-def poe_invtime_to_annual(imtls, poe, investigation_time):
+def poe_invtime_to_annual(poes, investigation_time):
     from numpy import array, log
     
     # now get annualised curves
-    P0 = 1 - array(imtls)
+    P0 = 1 - array(poe)
     n = -1*log(P0)
-    annual_probs = n / investigation_time
+    annual_poes = n / investigation_time
     
-    tmpdict[ut+'_probs_annual'] = annual_probs
-    tmpdict[ut+'_probs_invtime'] = hazcurve
+    return annual_poes
 
 def parse_oq_xml_poes(xmlfile):
     import xml.etree.ElementTree as ET
