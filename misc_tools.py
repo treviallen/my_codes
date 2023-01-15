c9f90515303bb964211dd2d5edbc5394e0bd2f0d
Trevor Allen <trevor.allen@ga.gov.au>
2023-01-16 10:21:14 +1100

    adding csv2json

--- misc_tools.py
+++ misc_tools.py
@@ -700,3 +700,31 @@ def get_ga_secondary_colours_2022():
     
 def get_line_styles():
     return ['-', '--', '-.', (0, (3, 5, 1, 5, 1, 5)), (0, (3, 1, 1, 1, 1, 1)), (0, (3, 5, 1, 5))]
+    
+    
+# function to convert csv to json
+def csv2json(csvFilePath, jsonFilePath):
+    '''
+    csvFilePath = input csv file
+    jsonFilePath = output json file
+    '''
+    
+    import csv 
+    import json 
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+          
